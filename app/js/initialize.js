f264f39761675097b93d0693d6d12d92dde5a06e
carlscelis <carlos_ccelis@hotmail.com>
2018-11-12 14:44:52 -0600

    Form: labels functionality done, working on off/on state of submit button.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -3,7 +3,20 @@
 // without waiting for stylesheets, images, and
 // subframes to finish loading
 document.addEventListener('DOMContentLoaded', function() {
-    // Do something
+    // Form interactions
+    	// Labeling
+		    // Add/remove class to ".input-fx" to change behavior of its own label
+				// If input is empty, <label> stays over input
+				// If input has text in it, <label> moves above input
+				$(".ac-input").focusout(function(){
+					$(".ac-input").blur(function(){
+						if( !this.value ) {
+							$(this).removeClass("filled");
+						} else{
+							$(this).addClass("filled");
+						}
+					});
+				});
 });
 
 
